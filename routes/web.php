934105e0d1e283095e6dc4d9b70e5a482871bcb1
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-12 09:16:10 +0800

    #06 Laravel Multi Auth with Breeze Part 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +30,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])
+    ->name('admin.dashboard');
+
+Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])
+    ->name('agent.dashboard');
