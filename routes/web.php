38f63d4a434a3ce58d0116f4cae7d97b54782b3e
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-13 10:32:35 +0800

    #14 Customize Login Form

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,8 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])
         ->name('agent.dashboard');
 }); // End Group Agent Middleware
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/login', [AdminController::class, 'AdminLogin'])
+        ->name('admin.login');
+});
