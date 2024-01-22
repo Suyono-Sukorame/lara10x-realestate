272fa4d9065f5d599b044f67afe7fd7d3897e839
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-22 15:14:35 +0800

    #34 Setup User Permission Part 1

---
+++ resources/views/backend/pages/permission/all_permission.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.admin_dashboard')
+
+@section('admin')
+    <div class="page-content">
+
+        <nav class="page-breadcrumb">
+            <ol class="breadcrumb">
+                <a href="{{ route('add.amenitie') }}" class="btn btn-inverse-info">Add Permission</a>
+            </ol>
+        </nav>
+
+        <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="card-title">Permission All</h6>
+
+                        <div class="table-responsive">
+                            <table id="dataTableExample" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>S1</th>
+                                        <th>Permission Name</th>
+                                        <th>Group Name</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($permissions as $key => $item)
+                                        <tr>
+                                            <td>{{ $key + 1 }}</td>
+                                            <td>{{ $item->name }}</td>
+                                            <td>{{ $item->group_name }}</td>
+                                            <td>
+                                                <a href="{{ route('edit.amenitie', $item->id) }}"
+                                                    class="btn btn-inverse-warning">Edit</a>
+                                                <form id="deleteForm_{{ $item->id }}"
+                                                    action="{{ route('delete.amenitie', $item->id) }}" method="POST"
+                                                    style="display: inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="button" class="btn btn-inverse-danger"
+                                                        onclick="confirmDelete('{{ $item->id }}')">Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+<script>
+    function confirmDelete(id) {
+        // Gunakan SweetAlert untuk menampilkan pemberitahuan konfirmasi
+        Swal.fire({
+            title: 'Apakah Anda yakin?',
+            text: "Anda tidak akan dapat mengembalikan ini!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Ya, hapus!',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Jika pengguna mengonfirmasi, kirim formulir untuk penghapusan
+                document.getElementById('deleteForm_' + id).submit();
+            }
+        });
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,116 +1,11 @@
 <?php
 
-// use App\Http\Controllers\ProfileController;
-// use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\AdminController;
-// use App\Http\Controllers\AgentController;
-// use App\Http\Controllers\Backend\PropertyTypeController;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// require __DIR__ . '/auth.php';
-
-// // Admin Group Middleware
-// Route::middleware(['auth', 'role:admin'])->group(function () {
-//     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])
-//         ->name('admin.dashboard');
-
-//     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])
-//         ->name('admin.logout');
-
-//     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])
-//         ->name('admin.profile');
-
-//     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])
-//         ->name('admin.profile.store');
-
-//     Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])
-//         ->name('admin.change.password');
-
-//     Route::post('/admin/update/password', [AdminController::class, 'AdminUpdatePassword'])
-//         ->name('admin.update.password');
-// }); // End Group Admin Middleware
-
-
-// // Agent Group Middleware
-// Route::middleware(['auth', 'role:agent'])->group(function () {
-//     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])
-//         ->name('agent.dashboard');
-// }); // End Group Agent Middleware
-
-
-// Route::get('/admin/login', [AdminController::class, 'AdminLogin'])
-//     ->name('admin.login');
-
-// // Property Type Routes
-// Route::middleware(['auth', 'role:admin'])->group(function () {
-
-//     // Property Type All Route
-//     Route::get('/all/type', [PropertyTypeController::class, 'AllType'])
-//         ->name('all.type');
-
-//     // Property Type Add Route
-//     Route::get('/add/type', [PropertyTypeController::class, 'AddType'])
-//         ->name('add.type');
-
-//     // Property Type Store Route
-//     Route::post('/store/type', [PropertyTypeController::class, 'StoreType'])
-//         ->name('store.type');
-
-//     // Property Type Edit Route
-//     Route::get('/edit/type/{id}', [PropertyTypeController::class, 'EditType'])
-//         ->name('edit.type');
-
-//     // Property Type Update Route
-//     Route::put('/update/type/{id}', [PropertyTypeController::class, 'UpdateType'])
-//         ->name('update.type');
-
-//     // Property Type Delete Route
-//     Route::delete('/delete/type/{id}', [PropertyTypeController::class, 'DeleteType'])
-//         ->name('delete.type');
-
-//     // Add more routes as needed
-
-// });
-
-// // Property Aminities Routes
-// Route::middleware(['auth', 'role:admin'])->group(function () {
-//     Route::get('/all/amenitie', [PropertyTypeController::class, 'AllAmenitie'])
-//         ->name('all.amenitie');
-
-//     Route::get('/add/amenitie', [PropertyTypeController::class, 'AddAmenitie'])
-//         ->name('add.amenitie');
-
-//     Route::post('/store/amenitie', [PropertyTypeController::class, 'StoreAmenitie'])
-//         ->name('store.amenitie');
-
-//     Route::get('/edit/amenitie/{id}', [PropertyTypeController::class, 'EditAmenitie'])
-//         ->name('edit.amenitie');
-
-//     Route::put('/update/amenitie', [PropertyTypeController::class, 'UpdateAmenitie'])
-//         ->name('update.amenitie');
-
-//     Route::delete('/delete/amenitie/{id}', [PropertyTypeController::class, 'DeleteAmenitie'])
-//         ->name('delete.amenitie');
-// });
-
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\Backend\PropertyTypeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\RoleController;
 
 // Public Routes
 Route::get('/', function () {
@@ -167,4 +62,16 @@ Route::middleware(['auth', 'role:admin'])->prefix('property')->group(function ()
     Route::get('/edit/amenitie/{id}', [PropertyTypeController::class, 'EditAmenitie'])->name('edit.amenitie');
     Route::put('/update/amenitie', [PropertyTypeController::class, 'UpdateAmenitie'])->name('update.amenitie');
     Route::delete('/delete/amenitie/{id}', [PropertyTypeController::class, 'DeleteAmenitie'])->name('delete.amenitie');
+});
+
+// Permission All Route
+Route::middleware(['auth', 'role:admin'])->prefix('property')->group(function () {
+    Route::get('/all/permission', [RoleController::class, 'AllPermission'])->name('all.permission');
+    Route::get('/add/type', [RoleController::class, 'AddType'])->name('add.type');
+    Route::post('/store/type', [RoleController::class, 'StoreType'])->name('store.type');
+    Route::get('/edit/type/{id}', [RoleController::class, 'EditType'])->name('edit.type');
+    Route::put('/update/type/{id}', [RoleController::class, 'UpdateType'])->name('update.type');
+    Route::delete('/delete/type/{id}', [PropertyTypeController::class, 'DeleteType'])->name('delete.type');
+
+    // end Permission All Route
 });
